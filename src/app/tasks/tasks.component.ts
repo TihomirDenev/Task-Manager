1a78a59e5328bd0de212bee10248a73c6f4650ca
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2024-09-09 23:05:26 +0300

    Complete Button Functionality

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -23,8 +23,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {}
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
